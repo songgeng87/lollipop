1316b19babb324e0946d8a750bc808bb9155276a
laura <zhiyazhao@126.com>
2024-09-25 16:02:17 +0800

    1.10
    update the images

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -94,7 +94,7 @@ export default function HomeLayout({ children }: { children: React.ReactNode }) 
                 <p className="text-[#0AEAF1]">NETWORK EXTENSION SERVICE</p>
                 <p>WITH <span className="text-[#FC15EF]">CRYPTOGRAPHIC</span></p>
                 <p className="text-[#FC15EF]">SETTLEMENT PROTOCOLS.</p>
-                <p className="text-xl font-thin mt-8">Customizable, Decentralized & Infrastructure Compatible</p>
+                <p className="text-xl font-thin mt-8">Customizable, Decentralized & Infrastructure Compatible </p>
             </div>
         </div>
         <div className="mt-16 px-16">
